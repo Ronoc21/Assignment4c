449d0dd6a33f094fcef4a1d81c8a42f0c773a90d
Conor <conor.okeefe.2121@gmail.com>
2021-04-21 20:44:54 -0700

    Hailstone.py

---
+++ hailstone.py
@@ -0,0 +1,14 @@
+# Conor Smith
+# Date 4/21/21
+# Description: defines function for hailstone sequence
+
+def hailstone(num):
+    answer = 1
+    """Takes a number (num) and applies hailstone formula if number is greater than one. If number is one program is complete"""
+    print(int(num))
+    if num > 1:
+        if num % 2 == 0:
+            answer += hailstone(num / 2)
+        else:
+            answer += hailstone((num * 3) + 1)
+    return answer
