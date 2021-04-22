2a7d7dcbb1eed1cbf5df7c3a485a83e2b4710741
Conor <conor.okeefe.2121@gmail.com>
2021-04-21 20:55:29 -0700

    Hailstone.py

--- hailstone.py
+++ hailstone.py
@@ -5,10 +5,12 @@
 def hailstone(num):
     answer = 0
     """Takes a number (num) and applies hailstone formula if number is greater than one. If number is one program is complete"""
+    print(int(num))
     if num > 1:
         if num % 2 == 0:
             answer += hailstone(num / 2)
         else:
             answer += hailstone((num * 3) + 1)
-    print(int(num))
     return answer
+
+result = hailstone(3)
